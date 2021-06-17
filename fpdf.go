f6f14b81ed416fd2186165ba7a89d258024223fd
Mikhail Slabchenko <mikhail_slabchenko@epam.com>
2021-06-17 12:41:15 +0300

    add getMultiCellHeight()

--- fpdf.go
+++ fpdf.go
@@ -2595,7 +2595,197 @@ func (f *Fpdf) SplitLines(txt []byte, w float64) [][]byte {
 // applications that use UTF-8 fonts and depend on having all trailing newlines
 // removed should call strings.TrimRight(txtStr, "\r\n") before calling this
 // method.
-func (f *Fpdf) MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill bool) int{
+func (f *Fpdf) MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill bool) {
+	if f.err != nil {
+		return
+	}
+	// dbg("MultiCell")
+	if alignStr == "" {
+		alignStr = "J"
+	}
+	cw := f.currentFont.Cw
+	if w == 0 {
+		w = f.w - f.rMargin - f.x
+	}
+	wmax := int(math.Ceil((w - 2*f.cMargin) * 1000 / f.fontSize))
+	s := strings.Replace(txtStr, "\r", "", -1)
+	srune := []rune(s)
+
+	// remove extra line breaks
+	var nb int
+	if f.isCurrentUTF8 {
+		nb = len(srune)
+		for nb > 0 && srune[nb-1] == '\n' {
+			nb--
+		}
+		srune = srune[0:nb]
+	} else {
+		nb = len(s)
+		bytes2 := []byte(s)
+
+		// for nb > 0 && bytes2[nb-1] == '\n' {
+
+		// Prior to August 2019, if s ended with a newline, this code stripped it.
+		// After that date, to be compatible with the UTF-8 code above, *all*
+		// trailing newlines were removed. Because this regression caused at least
+		// one application to break (see issue #333), the original behavior has been
+		// reinstated with a caveat included in the documentation.
+		if nb > 0 && bytes2[nb-1] == '\n' {
+			nb--
+		}
+		s = s[0:nb]
+	}
+	// dbg("[%s]\n", s)
+	var b, b2 string
+	b = "0"
+	if len(borderStr) > 0 {
+		if borderStr == "1" {
+			borderStr = "LTRB"
+			b = "LRT"
+			b2 = "LR"
+		} else {
+			b2 = ""
+			if strings.Contains(borderStr, "L") {
+				b2 += "L"
+			}
+			if strings.Contains(borderStr, "R") {
+				b2 += "R"
+			}
+			if strings.Contains(borderStr, "T") {
+				b = b2 + "T"
+			} else {
+				b = b2
+			}
+		}
+	}
+	sep := -1
+	i := 0
+	j := 0
+	l := 0
+	ls := 0
+	ns := 0
+	nl := 1
+	for i < nb {
+		// Get next character
+		var c rune
+		if f.isCurrentUTF8 {
+			c = srune[i]
+		} else {
+			c = rune(s[i])
+		}
+		if c == '\n' {
+			// Explicit line break
+			if f.ws > 0 {
+				f.ws = 0
+				f.out("0 Tw")
+			}
+
+			if f.isCurrentUTF8 {
+				newAlignStr := alignStr
+				if newAlignStr == "J" {
+					if f.isRTL {
+						newAlignStr = "R"
+					} else {
+						newAlignStr = "L"
+					}
+				}
+				f.CellFormat(w, h, string(srune[j:i]), b, 2, newAlignStr, fill, 0, "")
+			} else {
+				f.CellFormat(w, h, s[j:i], b, 2, alignStr, fill, 0, "")
+			}
+			i++
+			sep = -1
+			j = i
+			l = 0
+			ns = 0
+			nl++
+			if len(borderStr) > 0 && nl == 2 {
+				b = b2
+			}
+			continue
+		}
+		if c == ' ' || isChinese(c) {
+			sep = i
+			ls = l
+			ns++
+		}
+		if int(c) >= len(cw) {
+			f.err = fmt.Errorf("character outside the supported range: %s", string(c))
+			return
+		}
+		if cw[int(c)] == 0 { //Marker width 0 used for missing symbols
+			l += f.currentFont.Desc.MissingWidth
+		} else if cw[int(c)] != 65535 { //Marker width 65535 used for zero width symbols
+			l += cw[int(c)]
+		}
+		if l > wmax {
+			// Automatic line break
+			if sep == -1 {
+				if i == j {
+					i++
+				}
+				if f.ws > 0 {
+					f.ws = 0
+					f.out("0 Tw")
+				}
+				if f.isCurrentUTF8 {
+					f.CellFormat(w, h, string(srune[j:i]), b, 2, alignStr, fill, 0, "")
+				} else {
+					f.CellFormat(w, h, s[j:i], b, 2, alignStr, fill, 0, "")
+				}
+			} else {
+				if alignStr == "J" {
+					if ns > 1 {
+						f.ws = float64((wmax-ls)/1000) * f.fontSize / float64(ns-1)
+					} else {
+						f.ws = 0
+					}
+					f.outf("%.3f Tw", f.ws*f.k)
+				}
+				if f.isCurrentUTF8 {
+					f.CellFormat(w, h, string(srune[j:sep]), b, 2, alignStr, fill, 0, "")
+				} else {
+					f.CellFormat(w, h, s[j:sep], b, 2, alignStr, fill, 0, "")
+				}
+				i = sep + 1
+			}
+			sep = -1
+			j = i
+			l = 0
+			ns = 0
+			nl++
+			if len(borderStr) > 0 && nl == 2 {
+				b = b2
+			}
+		} else {
+			i++
+		}
+	}
+	// Last chunk
+	if f.ws > 0 {
+		f.ws = 0
+		f.out("0 Tw")
+	}
+	if len(borderStr) > 0 && strings.Contains(borderStr, "B") {
+		b += "B"
+	}
+	if f.isCurrentUTF8 {
+		if alignStr == "J" {
+			if f.isRTL {
+				alignStr = "R"
+			} else {
+				alignStr = ""
+			}
+		}
+		f.CellFormat(w, h, string(srune[j:i]), b, 2, alignStr, fill, 0, "")
+	} else {
+		f.CellFormat(w, h, s[j:i], b, 2, alignStr, fill, 0, "")
+	}
+	f.x = f.lMargin
+}
+
+// GetMultiCellHeight returns amount of cells, that multicell will contain
+func (f *Fpdf) GetMultiCellHeight(w, h float64, txtStr, borderStr, alignStr string, fill bool) int {
 	if f.err != nil {
 		return 0
 	}
@@ -2665,7 +2855,7 @@ func (f *Fpdf) MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill 
 	ls := 0
 	ns := 0
 	nl := 1
-	rowsAdded := 0
+	cellsCounter := 0
 	for i < nb {
 		// Get next character
 		var c rune
@@ -2690,11 +2880,9 @@ func (f *Fpdf) MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill 
 						newAlignStr = "L"
 					}
 				}
-				f.CellFormat(w, h, string(srune[j:i]), b, 2, newAlignStr, fill, 0, "")
-				rowsAdded++
+				cellsCounter++
 			} else {
-				f.CellFormat(w, h, s[j:i], b, 2, alignStr, fill, 0, "")
-				rowsAdded++
+				cellsCounter++
 			}
 			i++
 			sep = -1
@@ -2732,11 +2920,9 @@ func (f *Fpdf) MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill 
 					f.out("0 Tw")
 				}
 				if f.isCurrentUTF8 {
-					f.CellFormat(w, h, string(srune[j:i]), b, 2, alignStr, fill, 0, "")
-					rowsAdded++
+					cellsCounter++
 				} else {
-					f.CellFormat(w, h, s[j:i], b, 2, alignStr, fill, 0, "")
-					rowsAdded++
+					cellsCounter++
 				}
 			} else {
 				if alignStr == "J" {
@@ -2748,11 +2934,9 @@ func (f *Fpdf) MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill 
 					f.outf("%.3f Tw", f.ws*f.k)
 				}
 				if f.isCurrentUTF8 {
-					f.CellFormat(w, h, string(srune[j:sep]), b, 2, alignStr, fill, 0, "")
-					rowsAdded++
+					cellsCounter++
 				} else {
-					f.CellFormat(w, h, s[j:sep], b, 2, alignStr, fill, 0, "")
-					rowsAdded++
+					cellsCounter++
 				}
 				i = sep + 1
 			}
@@ -2784,15 +2968,13 @@ func (f *Fpdf) MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill 
 				alignStr = ""
 			}
 		}
-		f.CellFormat(w, h, string(srune[j:i]), b, 2, alignStr, fill, 0, "")
-		rowsAdded++
+		cellsCounter++
 	} else {
-		f.CellFormat(w, h, s[j:i], b, 2, alignStr, fill, 0, "")
-		rowsAdded++
+		cellsCounter++
 	}
 	f.x = f.lMargin
 
-	return rowsAdded
+	return cellsCounter
 }
 
 // write outputs text in flowing mode
